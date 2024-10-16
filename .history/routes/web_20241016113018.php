989aa9a4397a5421f428ca9b92add9e1a96c1ebe
João Paulo <37987074+joaopaulomirandamatias@users.noreply.github.com>
2024-10-16 11:44:14 -0300

    Projeto Notes Laravel

--- .history/routes/web_20241016113018.php
+++ .history/routes/web_20241016113018.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
    echo 'Hello World!';
 });
 
-Route::get('/main', [MainController::class, 'index']);
+Route::get('/about', function(){
+    echo "About rout";
+});
--- .history/routes/web_20241016113228.php
+++ .history/routes/web_20241016113228.php
@@ -1,10 +1,8 @@
 <?php
 
-use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
    echo 'Hello World!';
 });
 
-Route::get('/main', [MainController::class, 'index']);
